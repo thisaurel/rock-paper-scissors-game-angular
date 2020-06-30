52aed59aff43a08383e9935d9a2dc7e4b84b0337
Aurélien Riche <contact@aurelienriche.com>
2020-06-30 22:10:17 +0200

    Visual effects

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -9,16 +9,24 @@ import {ItemsInterface} from '../../interfaces/items.interface';
 })
 export class GameComponent implements OnInit {
 
+  public listOfChoices = this.gameService.items;
+
   constructor(
-    protected gameService: GameService,
+    public gameService: GameService,
   ) { }
 
   ngOnInit(): void {
   }
 
   public makeChoice(item: ItemsInterface): void {
+    this.visualChanges(item);
     this.gameService.callPlayerChoice(item);
     console.log(this.gameService.isPlayerWins());
+  }
+
+  public visualChanges(item: ItemsInterface): void {
+    this.listOfChoices.forEach((e) => e.selected = false);
+    console.log(item);
   }
 
 }
